39df18d3c13d8817d9754a155572a8704bf5ced5
DarrenCCT <sba22247@student.cct.ie>
2025-07-27 00:44:11 +0100

    Add files via upload
    Fixed indentation in the .py file. Some commenting added to the notebook.

--- elo.py
+++ elo.py
@@ -2,14 +2,14 @@
 
 def retrieve_elo(df, name):
     """
-        Retrieves an elo rating for a given name.
+    Retrieves an elo rating for a given name.
 
-        Parameters:
-        df (pandas dataframe): The dataset.
-        name (string): The player's name.
+    Parameters:
+    df (pandas dataframe): The dataset.
+    name (string): The player's name.
 
-        Returns: 
-        An integer that is the player's current elo rating.
+    Returns: 
+    An integer that is the player's current elo rating.
     """
     name = str(name)
     row = df.loc[df["Player_Map"] == name, "Elo"] #Return elo if player found
@@ -21,15 +21,15 @@ def retrieve_elo(df, name):
 
 
 def retrieve_highest_elo(df, name):
-     """
-        Retrieves the highest elo rating achieved for a given player name.
+    """
+    Retrieves the highest elo rating achieved for a given player name.
 
-        Parameters:
-        df (pandas dataframe): The dataset containing the column's "Player_Map" and "Highest Elo".
-        name (string): The player's name.
+    Parameters:
+    df (pandas dataframe): The dataset containing the column's "Player_Map" and "Highest Elo".
+    name (string): The player's name.
 
-        Returns: 
-        An integer that is the player's highest ever elo rating.
+    Returns: 
+    An integer that is the player's highest ever elo rating.
     """
     name = str(name)
     row = df.loc[df["Player_Map"] == name, "Highest Elo"]
@@ -41,38 +41,38 @@ def retrieve_highest_elo(df, name):
 
 def update_elo(df, col, index, elo):
     """
-        Updates a player's new adjusted elo rating after a match. Given the dataframe, index position, column, and elo rating.
-        Used for updating the match table dataframe.
+    Updates a player's new adjusted elo rating after a match. Given the dataframe, index position, column, and elo rating.
+    Used for updating the match table dataframe.
 
-        Parameters:
-        df (pandas dataframe): The dataset.
-        col (string): Column to be updated (e.g., "Player 1 Adjusted Elo")
-        index (integer): Position in the dataframe.
-        elo (integer): The player's elo.
+    Parameters:
+    df (pandas dataframe): The dataset.
+    col (string): Column to be updated (e.g., "Player 1 Adjusted Elo")
+    index (integer): Position in the dataframe.
+    elo (integer): The player's elo.
 
-        Returns: None.
-        This function does not return a value.
+    Returns: None.
+    This function does not return a value.
     """
     df.loc[index, col] = elo
 
 
 def expected_score(ratingA, ratingB):
     """
-        Calculates the expected score given two ratings.
-        Expected score for Player A is defined as:
+    Calculates the expected score given two ratings.
+    Expected score for Player A is defined as:
 
         E_A = 1/(1 + 10^((R_B-R_A)/400))
 
-        Where E_A is a positive number ranging from 0 to 1.
-        Representing Player A's odds of beating Player B.
+    Where E_A is a positive number ranging from 0 to 1.
+    Representing Player A's odds of beating Player B.
 
-        Player B's odds (E_B) can be calculated as 1 - E_A
+    Player B's odds (E_B) can be calculated as 1 - E_A
 
-        Parameters:
+    Parameters:
         ratingA (float): The elo rating of Player A.
         ratingB (float): The elo rating of Player B.
 
-        Returns:
+    Returns:
         A float between 0 and 1 that represents the expected score for Player A.
         Player B's expected score can be calculated as 1 - the returned value
     """
@@ -82,59 +82,59 @@ def expected_score(ratingA, ratingB):
 
 def adjusted_rating(k, rating, expectedScore, score):
     """
-        Calculates the adjusted elo rating after a set. 
-        Given the k-factor (k), rating to be adjusted (R_A), expected score (E_A), and actual score (S_A).
-        Adjusted rating is defined as:
+    Calculates the adjusted elo rating after a set. 
+    Given the k-factor (k), rating to be adjusted (R_A), expected score (E_A), and actual score (S_A).
+    Adjusted rating is defined as:
 
-        R'_A = R_A + k(S_A - E_A)
+    R'_A = R_A + k(S_A - E_A)
 
-        Parameters:
+    Parameters:
         k (integer): The K-Factor. 
         rating(float): The current rating to be adjusted.
         expectedScore(float): The expected score.
         score(float): The actual score.
 
-        Returns: 
+    Returns: 
         A float representing the updated elo rating after a match.
         The K-Factor determines the impact on the result of the rating.
-    """
+        """
     adjustedRating = rating + k*(score-expectedScore)
     return adjustedRating
 
 
 def update_main_elo_table(df, playerName, newElo):
     """
-        Updates the current rating in a pandas dataframe that contains the columns "Player_Map" and "Elo".
+    Updates the current rating in a pandas dataframe that contains the columns "Player_Map" and "Elo".
 
-        Parameters:
-        df (pandas dataframe): The dataset. 
-        playerName (string): The player's name.
-        newElo (integer): Current elo to be updated.
+    Parameters:
+    df (pandas dataframe): The dataset. 
+    playerName (string): The player's name.
+    newElo (integer): Current elo to be updated.
 
-        Returns: None.
-        This function does not return a value.
+    Returns: None.
+    This function does not return a value.
     """
     if playerName in df['Player_Map'].values:
         df.loc[df['Player_Map'] == playerName, 'Elo'] = newElo
     else:
         print(f"Player '{playerName}' not found in the dataframe.")
 
-# Updates highest ever elo
+
 def update_main_highest_elo_table(df, playerName, newElo):
     """
-        Updates the highest ever achieved elo rating for a player given the dataset, player name, and elo rating.
-        The dataframe should contain the columns "Player_Map" and "Highest_Elo".
+    Updates the highest ever achieved elo rating for a player given the dataset, player name, and elo rating.
+    The dataframe should contain the columns "Player_Map" and "Highest_Elo".
 
-        This function is only called if the player achieves a new highest rating.
-        Does not handle the check to see if the given score is higher than the entry in the dataframe.
+    This function is only called if the player achieves a new highest rating.
+    Does not handle the check to see if the given score is higher than the entry in the dataframe.
 
-        Parameters:
-        df (pandas dataframe): The dataset.
-        playerName (string): The player's name
-        newElo (integer): The player's highest elo.
+    Parameters:
+    df (pandas dataframe): The dataset.
+    playerName (string): The player's name
+    newElo (integer): The player's highest elo.
 
-        Returns: None.
-        This function does not return a value.
+    Returns: None.
+    This function does not return a value.
 
     """
     if playerName in df['Player_Map'].values:
